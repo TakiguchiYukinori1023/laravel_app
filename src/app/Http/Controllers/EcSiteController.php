9be859795e8a6a6a7ee090e627fde78974e53343
Yukinori Takiguchi <i.love.sid1994.1023@gmail.com>
2022-04-26 02:58:33 +0900

    auth導入

---
+++ src/app/Http/Controllers/EcSiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EcSiteController extends Controller
+{
+    public function topPage()
+    {
+        return view('/top');
+    }
+
+    public function search()
+    {
+        echo '赤さん';die();
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//以下は自分で作成したルート
+Route::get('/toppage', 'EcSiteController@topPage')->name('topPage');
+Route::post('/search', 'EcSiteController@search')->name('search');
