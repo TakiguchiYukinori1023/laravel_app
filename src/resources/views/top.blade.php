d5dce248cb6f81ccd38fe7301d481e59e9510b80
Yukinori Takiguchi <i.love.sid1994.1023@gmail.com>
2022-05-09 01:32:13 +0900

    親テンプレート、子テンプレートに分離

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <div class="flex-center position-ref full-height">
-    <a href="{{ route('login') }}">Login</a>
-    <a href="{{ route('register') }}">Register</a>
-  </div>
+@extends('layouts.base')
+
+@section('title', 'トップページ')
+
+@section('content')
+  <ul>
+    <li><a href="{{ route('login') }}">Login</a></li>
+    <li><a href="{{ route('register') }}">Register</a></li>
+  </ul>
+
   {{ Form::open(['route' => ['search'], 'method' => 'get']) }}
     {{ Form::text('text', null, ['placeholder' => 'キーワードを入力してください', 'size' => 30]) }}
     {{ Form::submit('検索', ['class'=>'submit'] )}}
   {{ Form::close() }}
-</body>
-</html>
+@endsection
